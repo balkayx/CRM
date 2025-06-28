6a9cc15ba983a4185867657f21ed8721f52e237d
balkayx <mrbalkayx@gmail.com>
2025-06-29 02:55:41 +0300

    Update insurance-crm.php

--- insurance-crm.php
+++ insurance-crm.php
@@ -10,9 +10,9 @@
  * Plugin Name: Insurance CRM
  * Plugin URI: https://github.com/anadolubirlik/insurance-crm
  * Description: Sigorta acenteleri için müşteri, poliçe ve görev yönetim sistemi.
- * Version: 1.9.5
+ * Version: 1.9.5_6
  * Pagename: insurance-crm.php
- * Page Version: 1.9.5
+ * Page Version: 1.9.5_6
  * Author: Mehmet BALKAY | Anadolu Birlik
  * Author URI: https://www.balkay.net
  */
